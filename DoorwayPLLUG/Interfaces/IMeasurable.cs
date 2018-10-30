935ae5a515868f87491b070bd0cbe0de1315eab9
Oleg <oleg.cs11@gmail.com>
2018-10-30 03:12:42 +0200

    Close #4

---
+++ DoorwayPLLUG/Interfaces/IMeasurable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoorwayPLLUG.Interfaces
+{
+    internal interface IMeasurable
+    {
+        double[] GetSmallestSurfaceDimensions();
+    }
+}
